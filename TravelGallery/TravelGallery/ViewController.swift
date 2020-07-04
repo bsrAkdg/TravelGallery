3a0a8e3f733c2999af8acbd4aa07a477c54655c0
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-07-04 15:43:00 +0300

    Project create

---
+++ TravelGallery/TravelGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelGallery
+//
+//  Created by Büşra Serdaroğlu on 4.07.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
